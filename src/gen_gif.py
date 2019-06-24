fcb2125b31633d180fb074cfe5370f12f4ac9417
Enric Moreu <enric.moreu.filella@gmail.com>
2019-06-24 11:30:13 +0100

    Update images

---
+++ src/gen_gif.py
@@ -0,0 +1,24 @@
+import imageio
+import IPython
+import glob
+
+anim_file = 'dcgan.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+  filenames = glob.glob('image*.png')
+  filenames = sorted(filenames)
+  last = -1
+  for i,filename in enumerate(filenames):
+    frame = 2*(i**0.5)
+    if round(frame) > round(last):
+      last = frame
+    else:
+      continue
+    image = imageio.imread(filename)
+    writer.append_data(image)
+  image = imageio.imread(filename)
+  writer.append_data(image)
+
+import IPython
+if IPython.version_info > (6,2,0,''):
+  display.Image(filename=anim_file)
